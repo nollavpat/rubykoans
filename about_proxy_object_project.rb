b825e590156966d3346081007796d13fe7e9a1eb
Patrick Vallon <jpatrickvallon@gmail.com>
2021-12-07 14:23:17 +0800

    Done!

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,35 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name
+      # add method name to messages
+      @messages << method_name
+
+      # call object method with given name and args
+      # this should be called last, so that object method return will be used
+      @object.send method_name, *args
+    else
+      super method_name, *args, &block
+    end
+  end
+
+  def called?(method_name)
+    @messages.include? method_name
+  end
+
+  def number_of_times_called(method_name)
+    @messages.count method_name
+  end
 end
 
 # The proxy object should pass the following Koan:
